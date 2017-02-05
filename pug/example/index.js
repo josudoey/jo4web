ebd50416bc1d6ca2cd5de4a0f4e1b3b99517d523
Joey Fang <josudoey@gmail.com>
2017-02-05 22:01:08 +0800

    [modify] fix example bug.

--- pug/example/index.js
+++ pug/example/index.js
@@ -1,5 +1,7 @@
+var path = require("path");
 var cons = require('consolidate');
+var fp = path.join(__dirname, "./template.pug");
 module.exports = function (opts, cb) {
-  return cons.pug("./template.pug", opts, cb);
+  return cons.pug(fp, opts, cb);
 }
 
