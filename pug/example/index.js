aee2afa427f48af9fca65b1e61c2e2f65a21d0d2
Joey Fang <josudoey@gmail.com>
2017-02-07 18:29:33 +0800

    [remove] split module

--- pug/example/index.js
+++
@@ -1,7 +0,0 @@
-var path = require("path");
-var cons = require('consolidate');
-var fp = path.join(__dirname, "./template.pug");
-module.exports = function (opts, cb) {
-  return cons.pug(fp, opts, cb);
-}
-
