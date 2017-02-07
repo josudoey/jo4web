f8227b65bb05e15dd70efbb3d1f21067807617c3
Joey Fang <josudoey@gmail.com>
2017-02-08 01:41:13 +0800

    [modify] fix self-signed bug

--- self-signed.js
+++ self-signed.js
@@ -11,22 +11,26 @@ var gererateSerialNumber = function () {
   var n = parseInt(sn[0], 16) - 8;
   return n.toString() + sn.substring(1);
 }
+
 module.exports = function (ctx) {
   ctx = ctx || {};
-  var keyPair = forge.pki.rsa.generateKeyPair(2048);
+  var keyPair = forge.pki.rsa.generateKeyPair(512);
   var cert = forge.pki.createCertificate();
   cert.serialNumber = gererateSerialNumber();
   var now = Date.now();
   var after = now + 365 * 24 * 60 * 60000
   cert.validity.notBefore = new Date(now);
   cert.validity.notAfter = new Date(after);
-  cert.publicKey = keyPair.publicKey;
   var attrs = [{
     name: 'commonName',
     value: 'localhost'
   }];
   cert.setSubject(attrs);
   cert.setIssuer(attrs);
+  cert.publicKey = keyPair.publicKey;
+  var md = forge.md.sha256.create();
+  cert.sign(keyPair.privateKey, md);
+
   ctx.key = forge.pki.privateKeyToPem(keyPair.privateKey);
   ctx.cert = forge.pki.certificateToPem(cert);
   return ctx;
