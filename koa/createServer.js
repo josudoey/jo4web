381323f309f112eb71825fa8ef9474819c7c51c0
Joey Fang <josudoey@gmail.com>
2017-02-09 23:25:56 +0800

    [remove] split module

--- koa/createServer.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (ctx) {
-  if (ctx.cert && ctx.key) {
-    var https = require("https");
-    return https.createServer(ctx, ctx.callback());
-  }
-  var http = require("http");
-  return http.createServer(ctx.callback());
-}
-
